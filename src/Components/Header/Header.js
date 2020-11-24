a56d257f6bbd4ffe800e54dc35a79dd1e6631e6f
digitalkoh <young@youngkoh.com>
2020-11-24 00:23:57 -0600

    Fine tuned nav

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,34 @@
+import React, { useState } from 'react';
 import './css/header.css';
+
+const showStyle={
+    top: "0",
+    transition: '.8s',
+    opacity: 1
+}
+const hideStyle={
+    top: "-300px",
+    transition: '.6s ease-in',
+    opacity: 0
+}
 
 function Header() {
 
-  return (
-    <>
-    <header className="App-header">
-        <h1>YKDD</h1>
-    </header>
+    const [visible, setVisible] = useState(hideStyle);
+    //const toggleVisible = () => setVisible(value => !value);
 
-    <nav>
-        <h2>YKDD</h2>
-        <div>Young Koh Design // Digital</div>
-    </nav>
-    </>
-  );
+    return (
+        <>
+            <header className="App-header">
+                <h1 onMouseOver={() => setVisible(showStyle)}>YKDD</h1>
+            </header>
+
+            <nav onMouseLeave={() => setVisible(hideStyle)}  style={visible}>
+                <h2>YKDD</h2>
+                <div>Young Koh Design // Digital</div>
+            </nav>
+        </>
+    );
 
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -17,6 +17,15 @@ function Home() {
         <p>
             Fusce sit amet sapien sollicitudin, feugiat tortor ut, pellentesque justo. Maecenas varius feugiat massa, a ornare dui placerat ut. Etiam placerat sapien eu enim cursus condimentum. Mauris eu aliquet turpis. In in urna porttitor, molestie dui non, suscipit elit. Nullam ornare nec ipsum sit amet feugiat. Praesent ac nulla egestas, porttitor urna vitae, mollis nulla.
         </p>
+        <p>
+            Sed convallis tincidunt tristique. Duis ullamcorper purus urna, lacinia posuere lectus volutpat volutpat. Suspendisse faucibus metus id arcu dictum porta. Donec congue mollis orci in ullamcorper. Aliquam tincidunt facilisis congue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus nec mattis elit. Integer est ex, facilisis et diam vitae, tristique tempus nunc.
+        </p>
+        <p>
+            Mauris in molestie nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque elementum, tortor ac ornare eleifend, justo enim feugiat tellus, a porta nibh nisl id massa. In hac habitasse platea dictumst. Aliquam erat volutpat. Nullam dictum egestas velit, ac congue ipsum egestas vitae. In quis sollicitudin tortor. Duis sollicitudin ligula felis, sed viverra leo viverra a. Nam eget enim faucibus, euismod risus vel, condimentum augue. Proin tempus quam quis volutpat eleifend. Vivamus at sollicitudin ante. Integer sed neque congue, rutrum ante egestas, vehicula quam. Pellentesque lacinia nisi metus, vel vestibulum nunc tempus non. Curabitur a velit odio. Phasellus nec enim quis ligula tristique pellentesque ac dignissim urna.
+        </p>
+        <p>
+            Fusce sit amet sapien sollicitudin, feugiat tortor ut, pellentesque justo. Maecenas varius feugiat massa, a ornare dui placerat ut. Etiam placerat sapien eu enim cursus condimentum. Mauris eu aliquet turpis. In in urna porttitor, molestie dui non, suscipit elit. Nullam ornare nec ipsum sit amet feugiat. Praesent ac nulla egestas, porttitor urna vitae, mollis nulla.
+        </p>
       </div>
   );
 
